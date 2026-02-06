9a52ee9d601e0d8ae1ddb8ebf6dc69a57ff8070e
gurwan <gurwan.thomas56190@gmail.com>
2026-02-06 12:36:03 +0100

    g

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'guid',
-        'first_name', // Au lieu de prenom
-        'last_name',  // Au lieu de nom
+        'prenom',
+        'nom',
         'email',
         'service_id',
     ];
+
     public function service()
     {
         return $this->belongsTo(Service::class);
     }
-
 }
